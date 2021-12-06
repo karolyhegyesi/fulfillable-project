c826d66fa75525b16fe973298e3cca55c2c29592
Károly Hegyesi <karoly.hegyesi@3dhistech.com>
2021-12-06 11:17:56 +0100

    fulfillabe

---
+++ fulfillable/fulfillable/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace fulfillable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs s)
+        {
+           
+                listBox1.Items.Clear();
+                Tuple<string,List<OrdersHelper.OrdersData>> orders = OrdersHelper.Get_orders(tBOrder.Text);
+                if (orders.Item2 != null)
+                {
+                    listBox1.Items.Add(string.Concat(("product_id").PadRight(20, ' '), ("quantity").PadRight(25, ' '), ("priority").PadRight(20, ' '), "created_at"));
+                    foreach (var data in orders.Item2)
+                    {
+                        string Lines = String.Concat((data.product_id.PadRight(30, ' ')),
+                                       (data.quantity.ToString().PadRight(30, ' ')),
+                                       (data.priorityName.PadRight(30, ' ')),
+                                        data.created_at.ToString());
+                        listBox1.Items.Add(Lines);
+                    }
+
+
+                }
+                else listBox1.Items.Add(orders.Item1);
+           
+        }
+    }
+}
---
+++ fulfillable/fulfillable/OrdersHelper.cs
@@ -0,0 +1,118 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+namespace fulfillable
+{
+    class OrdersHelper
+    {
+
+        public static Tuple<string ,List<OrdersData>> Get_orders(string json_str)
+        {
+            string errorMsg = string.Empty;
+            try
+            {
+                Dictionary<string, int> params_ok = CheckParameter(json_str.Trim(' '));
+                if (params_ok != null)
+                {
+                    return new Tuple<string, List<OrdersData>>(errorMsg, Getlist(params_ok));
+                }
+                else errorMsg= ("Invalid input parameters!") ;
+            }
+            catch (FileNotFoundException e) { errorMsg = e.Message; }
+            catch (FormatException e) { errorMsg = string.Concat("csv fileError: ", e.Message); }
+            return new Tuple<string, List<OrdersData>>(errorMsg, null); 
+        }
+        
+        private static Dictionary<string, int> CheckParameter(string json_str)
+        {
+            Dictionary<string, int> ret = new Dictionary<string, int>();
+            try
+            {
+                if (!string.IsNullOrEmpty(json_str))
+                {
+                    var paramTree = JsonConvert.DeserializeObject<JObject>(json_str);
+                    foreach (var item in paramTree.Properties())
+                    {
+                        ret.Add(item.Name, Int32.Parse(item.Value.ToObject<object>().ToString()));
+                    }
+                } else ret = null;            
+            }
+            catch { ret = null; }
+
+            return ret;
+        }
+
+        public  class OrdersData
+        {
+            public string product_id { get; set; }
+            public int quantity { get; set; }
+            public int priority { get; set; }
+            public string priorityName { get; set; }
+            public DateTime created_at { get; set; }
+
+        }
+
+        
+        private static List<OrdersData> Getlist(Dictionary<string, int> orders)
+        {
+            List<OrdersData> ordesListd_sort = new List<OrdersData>();
+            string Setpriority(int prior)
+            {
+                string ret = string.Empty;
+                switch (prior)
+                {
+                    case 1: ret = "low"; break;
+                    case 2: ret = "medium"; break; 
+                    case 3: ret = "high"; break;
+                    default: ret = "no data"; break;
+                }
+                return ret;
+            }
+
+            try
+            {
+                using (var reader = new StreamReader(@"orders.csv"))
+                {
+
+                    List<OrdersData> ordesList = new List<OrdersData>();
+                    int counter = 1;
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        if (counter > 1)
+                        {
+                            var values = line.Split(',');
+                            try
+                            {
+                                if (orders.Any(p => p.Key == values[0] && p.Value >= int.Parse(values[1])))
+                                {
+                                    OrdersData item = new OrdersData();
+                                    item.product_id = values[0];
+                                    item.quantity = int.Parse(values[1]);
+                                    item.priority = int.Parse(values[2]);
+                                    item.priorityName = Setpriority(int.Parse(values[2]));
+                                    item.created_at = DateTime.Parse(values[3]);
+                                    ordesList.Add(item);
+                                }
+                            }
+                            finally {}
+                        }
+                        counter++;
+                    }
+                    ordesListd_sort = ordesList.OrderBy(x => x.created_at).OrderByDescending(x => x.priority).ToList();
+                }
+            }
+            
+            catch (FormatException e) { throw; }
+            return ordesListd_sort;
+        }
+    }
+}
